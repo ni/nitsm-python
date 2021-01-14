1235a3718c71abc9054653b6ad528eae6117d69e
smooresni <sean.moore@ni.com>
2021-01-14 11:39:14 -0600

    make the type library accessible at the module level

--- src/nitsm/unit_tests/conftest.py
+++ src/nitsm/unit_tests/conftest.py
@@ -5,6 +5,16 @@ import win32com.client.selecttlb
 import nitsm.codemoduleapi
 
 
+def _find_standalone_tsm_context_tlb():
+    tlbs = win32com.client.selecttlb.FindTlbsWithDescription(
+        "NI TestStand Semiconductor Module Standalone Semiconductor Module Context"
+    )
+    return tlbs[0]
+
+
+standalone_tsm_context_tlb = _find_standalone_tsm_context_tlb()
+
+
 @pytest.fixture
 def _published_data_reader_factory(request):
     # get absolute path of the pin map file which is assumed to be relative to the test module
@@ -25,12 +35,7 @@ def standalone_tsm_context(_published_data_reader_factory):
 
 @pytest.fixture
 def published_data_reader(_published_data_reader_factory):
-    typelibs = win32com.client.selecttlb.FindTlbsWithDescription(
-        "NI TestStand Semiconductor Module Standalone Semiconductor Module Context"
-    )
-    standalone_tsm_typelib = typelibs[0]
-
     published_data_reader = win32com.client.CastTo(
-        _published_data_reader_factory[1], "IPublishedDataReader", standalone_tsm_typelib
+        _published_data_reader_factory[1], "IPublishedDataReader", standalone_tsm_context_tlb
     )
     return published_data_reader
