3e8366d69e385ed616fc898267d0e3cb27938369
NI\chmitng <chien.mien.tng@ni.com>
2021-01-18 11:58:42 +0800

    Update test_nidaqmx.py
    -Remove additional @pytest.fixture
    -Combine and remove additional pin_map_dut_pins and pin_map_system_pins
    -Change  get_all_nidaqmx_tasks("") to return all tasks regardless of task type
    -pass "" empty string in task type to return task names  of all task type

--- src/nitsm/unit_tests/test_nidaqmx.py
+++ src/nitsm/unit_tests/test_nidaqmx.py
@@ -8,33 +8,10 @@ from nitsm.codemoduleapi.pinquerycontexts import NIDAQmxMultiplePinMultipleTaskQ
 
 
 @pytest.fixture
-def simulated_nidaqmx_tasks1(standalone_tsm_context):
-    task_names, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("ai")
-    tasks = [nidaqmx.Task(tsk_name) for tsk_name in task_names]
-    for task_name, task in zip(task_names, tasks):
-        standalone_tsm_context.set_nidaqmx_task(task_name, task)
-    yield tasks
-    for task in tasks:
-        task.close()
-
-
-@pytest.fixture
-def simulated_nidaqmx_tasks2(standalone_tsm_context):
-    task_names, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("ao")
-    tasks = [nidaqmx.Task(tsk_name) for tsk_name in task_names]
-    for task_name, task in zip(task_names, tasks):
-        standalone_tsm_context.set_nidaqmx_task(task_name, task)
-    yield tasks
-    for task in tasks:
-        task.close()
-
-
-@pytest.fixture
 def simulated_nidaqmx_tasks(standalone_tsm_context):
-    task_names1, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("ai")
-    task_names2, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("ao")
-    tasks = [nidaqmx.Task(tsk_name) for tsk_name in task_names1 + task_names2]
-    for task_name, task in zip(task_names1 + task_names2, tasks):
+    task_names, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("")
+    tasks = [nidaqmx.Task(tsk_name) for tsk_name in task_names]
+    for task_name, task in zip(task_names, tasks):
         standalone_tsm_context.set_nidaqmx_task(task_name, task)
     yield tasks
     for task in tasks:
@@ -43,55 +20,37 @@ def simulated_nidaqmx_tasks(standalone_tsm_context):
 
 @pytest.mark.pin_map("nidaqmx.pinmap")
 class TestNIDAQmx:
-    pin_map_instruments = ["DAQmx1", "DAQmx2", "DAQmx3"]
-    pin_map_task_types = ["ai", "ao"]
-    pin_map_dut_pins1 = ["DUTPin1"]
-    pin_map_dut_pins2 = ["DUTPin2"]
-    pin_map_system_pins1 = ["SystemPin1"]
-    pin_map_system_pins2 = ["SystemPin2"]
+    pin_map_instruments = ["DAQmx1", "DAQmx2"]
+    pin_map_dut_pins = ["DUTPin1", "DUTPin2"]
+    pin_map_system_pins = ["SystemPin1"]
     pin_map_pin_groups = ["PinGroup1"]
 
     def test_get_all_nidaqmx_task_names(self, standalone_tsm_context: SemiconductorModuleContext):
-        for task_type in self.pin_map_task_types:
-            (
-                task_names,
-                channel_lists,
-            ) = standalone_tsm_context.get_all_nidaqmx_task_names(task_type)
-            assert isinstance(task_names, tuple)
-            assert isinstance(channel_lists, tuple)
-            assert len(task_names) == len(channel_lists)
-            for task_name, channel_list in zip(task_names, channel_lists):
-                assert isinstance(task_name, str)
-                assert isinstance(channel_list, str)
-                assert task_name in self.pin_map_instruments
+        task_names, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("")
+        assert isinstance(task_names, tuple)
+        assert isinstance(channel_lists, tuple)
+        assert len(task_names) == len(channel_lists)
+        for task_name, channel_list in zip(task_names, channel_lists):
+            assert isinstance(task_name, str)
+            assert isinstance(channel_list, str)
+            assert task_name in self.pin_map_instruments
 
     def test_set_nidaqmx_task(self, standalone_tsm_context: SemiconductorModuleContext):
-        for task_type in self.pin_map_task_types:
-            (
-                task_names,
-                channel_lists,
-            ) = standalone_tsm_context.get_all_nidaqmx_task_names(task_type)
-            for task_name, channel_list in zip(task_names, channel_lists):
-                with nidaqmx.Task(task_name) as task:
-                    standalone_tsm_context.set_nidaqmx_task(task_name, task)
-                    assert SemiconductorModuleContext._sessions[id(task)] is task
+        task_names, channel_lists = standalone_tsm_context.get_all_nidaqmx_task_names("")
+        for task_name, channel_list in zip(task_names, channel_lists):
+            with nidaqmx.Task(task_name) as task:
+                standalone_tsm_context.set_nidaqmx_task(task_name, task)
+                assert SemiconductorModuleContext._sessions[id(task)] is task
 
-    def test_get_all_nidaqmx_tasks1(self, standalone_tsm_context, simulated_nidaqmx_tasks1):
-        queried_tasks = standalone_tsm_context.get_all_nidaqmx_tasks("ai")
+    def test_get_all_nidaqmx_tasks(self, standalone_tsm_context, simulated_nidaqmx_tasks):
+        queried_tasks = standalone_tsm_context.get_all_nidaqmx_tasks("")
         for queried_task in queried_tasks:
             assert isinstance(queried_task, nidaqmx.Task)
-            assert queried_task in simulated_nidaqmx_tasks1
-        assert len(queried_tasks) == len(simulated_nidaqmx_tasks1)
-
-    def test_get_all_nidaqmx_tasks2(self, standalone_tsm_context, simulated_nidaqmx_tasks2):
-        queried_tasks = standalone_tsm_context.get_all_nidaqmx_tasks("ao")
-        for queried_task in queried_tasks:
-            assert isinstance(queried_task, nidaqmx.Task)
-            assert queried_task in simulated_nidaqmx_tasks2
-        assert len(queried_tasks) == len(simulated_nidaqmx_tasks2)
+            assert queried_task in simulated_nidaqmx_tasks
+        assert len(queried_tasks) == len(simulated_nidaqmx_tasks)
 
     def test_pin_to_nidaqmx_task(self, standalone_tsm_context, simulated_nidaqmx_tasks):
-        for system_pin in self.pin_map_system_pins1:
+        for system_pin in self.pin_map_system_pins:
             (
                 pin_query_context,
                 queried_task,
@@ -117,19 +76,18 @@ class TestNIDAQmx:
                 assert queried_task in simulated_nidaqmx_tasks
 
     def test_pins_to_nidaqmx_task(self, standalone_tsm_context, simulated_nidaqmx_tasks):
-        all_pins = self.pin_map_dut_pins1 + self.pin_map_system_pins1
         (
             pin_query_context,
             queried_task,
             queried_channel_list,
-        ) = standalone_tsm_context.pins_to_nidaqmx_task(all_pins)
+        ) = standalone_tsm_context.pins_to_nidaqmx_task(self.pin_map_dut_pins)
         assert isinstance(pin_query_context, NIDAQmxMultiplePinSingleTaskQueryContext)
         assert isinstance(queried_task, nidaqmx.Task)
         assert isinstance(queried_channel_list, str)
         assert queried_task in simulated_nidaqmx_tasks
 
     def test_pins_to_nidaqmx_tasks(self, standalone_tsm_context, simulated_nidaqmx_tasks):
-        all_pins = self.pin_map_dut_pins2 + self.pin_map_system_pins2
+        all_pins = self.pin_map_dut_pins + self.pin_map_system_pins
         (
             pin_query_context,
             queried_tasks,
