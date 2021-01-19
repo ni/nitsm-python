cc8ef42aaa6cfd9c997cf049ef96dcfe426900d6
smooresni <sean.moore@ni.com>
2021-01-19 15:03:51 -0600

    remove unnecessary looping

--- tests/test_nidmm.py
+++ tests/test_nidmm.py
@@ -25,7 +25,6 @@ class TestNIDMM:
     pin_map_instruments = ["DMM1", "DMM2", "DMM3"]
     pin_map_dut_pins = ["DUTPin1"]
     pin_map_system_pins = ["SystemPin1"]
-    pin_map_pin_groups = ["PinGroup1"]
 
     def test_get_all_nidmm_instrument_names(
         self, standalone_tsm_context: SemiconductorModuleContext
@@ -48,6 +47,7 @@ class TestNIDMM:
         self, standalone_tsm_context: SemiconductorModuleContext, simulated_nidmm_sessions
     ):
         queried_sessions = standalone_tsm_context.get_all_nidmm_sessions()
+        assert isinstance(queried_sessions, tuple)
         assert len(queried_sessions) == len(simulated_nidmm_sessions)
         for queried_session in queried_sessions:
             assert isinstance(queried_session, nidmm.Session)
@@ -56,26 +56,24 @@ class TestNIDMM:
     def test_pin_to_nidmm_session(
         self, standalone_tsm_context: SemiconductorModuleContext, simulated_nidmm_sessions
     ):
-        for system_pin in self.pin_map_system_pins:
-            pin_query_context, queried_session = standalone_tsm_context.pin_to_nidmm_session(
-                system_pin
-            )
-            assert isinstance(pin_query_context, NIDmmSinglePinSingleSessionQueryContext)
-            assert isinstance(queried_session, nidmm.Session)
-            assert queried_session in simulated_nidmm_sessions
+        pin_query_context, queried_session = standalone_tsm_context.pin_to_nidmm_session(
+            "SystemPin1"
+        )
+        assert isinstance(pin_query_context, NIDmmSinglePinSingleSessionQueryContext)
+        assert isinstance(queried_session, nidmm.Session)
+        assert queried_session in simulated_nidmm_sessions
 
     def test_pin_to_nidmm_sessions(
         self, standalone_tsm_context: SemiconductorModuleContext, simulated_nidmm_sessions
     ):
-        for pin_group in self.pin_map_pin_groups:
-            pin_query_context, queried_sessions = standalone_tsm_context.pin_to_nidmm_sessions(
-                pin_group
-            )
-            assert isinstance(pin_query_context, NIDmmSinglePinMultipleSessionQueryContext)
-            assert isinstance(queried_sessions, tuple)
-            for queried_session in queried_sessions:
-                assert isinstance(queried_session, nidmm.Session)
-                assert queried_session in simulated_nidmm_sessions
+        pin_query_context, queried_sessions = standalone_tsm_context.pin_to_nidmm_sessions(
+            "PinGroup1"
+        )
+        assert isinstance(pin_query_context, NIDmmSinglePinMultipleSessionQueryContext)
+        assert isinstance(queried_sessions, tuple)
+        for queried_session in queried_sessions:
+            assert isinstance(queried_session, nidmm.Session)
+            assert queried_session in simulated_nidmm_sessions
 
     def test_pins_to_nidmm_sessions(
         self, standalone_tsm_context: SemiconductorModuleContext, simulated_nidmm_sessions
