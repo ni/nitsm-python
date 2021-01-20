30e8e2dfb20dc8ffd5de17edd632a48234173f2f
smooresni <sean.moore@ni.com>
2021-01-20 07:19:38 -0600

    add fixtures so tests will pass

--- tests/test_site_and_global_data.py
+++ tests/test_site_and_global_data.py
@@ -1,33 +1,33 @@
 import pytest
 
-from nitsm.codemoduleapi import SemiconductorModuleContext
-
 
 @pytest.mark.pin_map("site_and_global_data.pinmap")
 class TestSiteAndGlobalData:
-    data_id = "data_id"
-    data = "2021"
+    site_data_id = "site_data_id"
+    site_data = ["IES"]
+    global_data_id = "global_data_id"
+    global_data = "NI"
 
-    def test_set_site_data(self, standalone_tsm_context: SemiconductorModuleContext):
-        standalone_tsm_context.set_site_data(self.data_id, [self.data])
+    @pytest.fixture
+    def simulated_site_data(self, standalone_tsm_context):
+        standalone_tsm_context.set_site_data(self.site_data_id, self.site_data)
 
-    def test_get_site_data(self, standalone_tsm_context: SemiconductorModuleContext):
-        queried_datas = standalone_tsm_context.get_site_data(self.data_id)
-        assert isinstance(queried_datas, tuple)
-        for queried_data in queried_datas:
-            assert isinstance(queried_data, str)
-            assert queried_data in self.data
+    def test_get_site_data(self, standalone_tsm_context, simulated_site_data):
+        queried_data = standalone_tsm_context.get_site_data(self.site_data_id)
+        assert isinstance(queried_data, tuple)
+        for data in queried_data:
+            assert data in self.site_data
 
-    def test_site_data_exists(self, standalone_tsm_context: SemiconductorModuleContext):
-        assert standalone_tsm_context.site_data_exists(self.data_id)
+    def test_site_data_exists(self, standalone_tsm_context, simulated_site_data):
+        assert standalone_tsm_context.site_data_exists(self.site_data_id)
 
-    def test_set_global_data(self, standalone_tsm_context: SemiconductorModuleContext):
-        standalone_tsm_context.set_global_data(self.data_id, self.data)
+    @pytest.fixture
+    def simulated_global_data(self, standalone_tsm_context):
+        standalone_tsm_context.set_global_data(self.global_data_id, self.global_data)
 
-    def test_get_global_data(self, standalone_tsm_context: SemiconductorModuleContext):
-        queried_data = standalone_tsm_context.get_global_data(self.data_id)
-        assert isinstance(queried_data, str)
-        assert queried_data in self.data
+    def test_get_global_data(self, standalone_tsm_context, simulated_global_data):
+        queried_data = standalone_tsm_context.get_global_data(self.global_data_id)
+        assert queried_data == self.global_data
 
-    def test_global_data_exists(self, standalone_tsm_context: SemiconductorModuleContext):
-        assert standalone_tsm_context.global_data_exists(self.data_id)
+    def test_global_data_exists(self, standalone_tsm_context, simulated_global_data):
+        assert standalone_tsm_context.global_data_exists(self.global_data_id)
