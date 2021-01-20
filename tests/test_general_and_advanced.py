08e79e161cd1235686b97454b34d086e41527190
smooresni <sean.moore@ni.com>
2021-01-20 10:08:00 -0600

    refactor unit tests for new tsmcontext and pin map

--- tests/test_general_and_advanced.py
+++ tests/test_general_and_advanced.py
@@ -1,36 +1,30 @@
 import pytest
-
-from nitsm.codemoduleapi import SemiconductorModuleContext
+from nitsm.codemoduleapi import Capability, InstrumentTypeIdConstants
 
 
 @pytest.mark.pin_map("general_and_advanced.pinmap")
 class TestGeneralAndAdvanced:
-    capability = ""
     pin_map_dut_pins = ["DUTPin1"]
     pin_map_system_pins = ["SystemPin1"]
-    pin_map_pin_group = "PinGroup1"
-    pin_map_pin_groups = ["PinGroup1"]
-    pin_map_instrument_type_id = "Relay1_Id"
 
-    def test_get_pin_names(self, standalone_tsm_context: SemiconductorModuleContext):
-        dut_pins, system_pins = standalone_tsm_context.get_pin_names(
-            self.pin_map_instrument_type_id, self.capability
+    def test_get_pin_names(self, standalone_tsm_context):
+        queried_dut_pins, queried_system_pins = standalone_tsm_context.get_pin_names(
+            InstrumentTypeIdConstants.ANY, Capability.ALL
         )
-        assert isinstance(dut_pins, tuple)
-        assert isinstance(system_pins, tuple)
-        assert len(dut_pins) == len(self.pin_map_dut_pins)
-        assert len(system_pins) == len(self.pin_map_system_pins)
-        for dut_pin, system_pin in zip(dut_pins, system_pins):
+        assert isinstance(queried_dut_pins, tuple)
+        assert isinstance(queried_system_pins, tuple)
+        assert len(queried_dut_pins) == len(self.pin_map_dut_pins)
+        assert len(queried_system_pins) == len(self.pin_map_system_pins)
+        for dut_pin in queried_dut_pins:
             assert isinstance(dut_pin, str)
-            assert isinstance(system_pin, str)
             assert dut_pin in self.pin_map_dut_pins
+        for system_pin in queried_system_pins:
+            assert isinstance(system_pin, str)
             assert system_pin in self.pin_map_system_pins
 
-    def test_filter_pins_by_instrument_type(
-        self, standalone_tsm_context: SemiconductorModuleContext
-    ):
+    def test_filter_pins_by_instrument_type(self, standalone_tsm_context):
         filtered_pins = standalone_tsm_context.filter_pins_by_instrument_type(
-            self.pin_map_dut_pins, self.pin_map_instrument_type_id, self.capability
+            self.pin_map_dut_pins, "CustomInstrumentTypeId1", Capability.ALL
         )
         assert isinstance(filtered_pins, tuple)
         assert len(filtered_pins) == len(self.pin_map_dut_pins)
@@ -38,18 +32,20 @@ class TestGeneralAndAdvanced:
             assert isinstance(filtered_pin, str)
             assert filtered_pin in self.pin_map_dut_pins
 
-    def test_get_pins_in_pin_group(self, standalone_tsm_context: SemiconductorModuleContext):
-        pins = standalone_tsm_context.get_pins_in_pin_group(self.pin_map_pin_group)
-        assert isinstance(pins, tuple)
-        assert len(pins) == len(self.pin_map_dut_pins + self.pin_map_system_pins)
-        for pin in pins:
-            assert isinstance(pin, str)
-            assert pin in self.pin_map_dut_pins + self.pin_map_system_pins
+    def test_get_pins_in_pin_group(self, standalone_tsm_context):
+        pin_group_pins = self.pin_map_dut_pins + self.pin_map_system_pins
+        queried_pins = standalone_tsm_context.get_pins_in_pin_group("PinGroup1")
+        assert isinstance(queried_pins, tuple)
+        assert len(queried_pins) == len(pin_group_pins)
+        for queried_pin in queried_pins:
+            assert isinstance(queried_pin, str)
+            assert queried_pin in pin_group_pins
 
-    def test_get_pins_in_pin_groups(self, standalone_tsm_context: SemiconductorModuleContext):
-        pins = standalone_tsm_context.get_pins_in_pin_groups(self.pin_map_pin_groups)
-        assert isinstance(pins, tuple)
-        assert len(pins) == len(self.pin_map_dut_pins + self.pin_map_system_pins)
-        for pin in pins:
-            assert isinstance(pin, str)
-            assert pin in self.pin_map_dut_pins + self.pin_map_system_pins
+    def test_get_pins_in_pin_groups(self, standalone_tsm_context):
+        pin_group_pins = self.pin_map_dut_pins + self.pin_map_system_pins
+        queried_pins = standalone_tsm_context.get_pins_in_pin_groups(["PinGroup1"])
+        assert isinstance(queried_pins, tuple)
+        assert len(queried_pins) == len(pin_group_pins)
+        for queried_pin in queried_pins:
+            assert isinstance(queried_pin, str)
+            assert queried_pin in pin_group_pins
