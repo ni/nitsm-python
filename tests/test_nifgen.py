b9483848cd465a90dda1e3b867462f2817e0907e
smooresni <sean.moore@ni.com>
2021-01-20 16:43:13 -0600

    clean up

--- tests/test_nifgen.py
+++ tests/test_nifgen.py
@@ -1,7 +1,5 @@
 import nifgen
 import pytest
-
-from nitsm.codemoduleapi import SemiconductorModuleContext
 from nitsm.codemoduleapi.pinquerycontexts import NIFGenSinglePinSingleSessionQueryContext
 from nitsm.codemoduleapi.pinquerycontexts import NIFGenSinglePinMultipleSessionQueryContext
 from nitsm.codemoduleapi.pinquerycontexts import NIFGenMultiplePinSingleSessionQueryContext
@@ -11,13 +9,7 @@ from nitsm.codemoduleapi.pinquerycontexts import NIFGenMultiplePinMultipleSessio
 @pytest.fixture
 def simulated_nifgen_sessions(standalone_tsm_context):
     instrument_names = standalone_tsm_context.get_all_nifgen_instrument_names()
-    sessions = [
-        nifgen.Session(
-            instrument_name,
-            options={"Simulate": True, "DriverSetup": {"Model": "5451", "BoardType": "PXIe"}},
-        )
-        for instrument_name in instrument_names
-    ]
+    sessions = [nifgen.Session("", options={"Simulate": True}) for _ in instrument_names]
     for instrument_name, session in zip(instrument_names, sessions):
         standalone_tsm_context.set_nifgen_session(instrument_name, session)
     yield sessions
@@ -31,9 +23,7 @@ class TestNIFGen:
     pin_map_dut_pins = ["DUTPin1"]
     pin_map_system_pins = ["SystemPin1", "SystemPin2"]
 
-    def test_get_all_nifgen_instrument_names(
-        self, standalone_tsm_context: SemiconductorModuleContext
-    ):
+    def test_get_all_nifgen_instrument_names(self, standalone_tsm_context):
         instrument_names = standalone_tsm_context.get_all_nifgen_instrument_names()
         assert isinstance(instrument_names, tuple)
         assert len(instrument_names) == len(self.pin_map_instruments)
@@ -41,42 +31,32 @@ class TestNIFGen:
             assert isinstance(instrument_name, str)
             assert instrument_name in self.pin_map_instruments
 
-    def test_set_nifgen_session(self, standalone_tsm_context: SemiconductorModuleContext):
+    def test_set_nifgen_session(self, standalone_tsm_context):
         instrument_names = standalone_tsm_context.get_all_nifgen_instrument_names()
         for instrument_name in instrument_names:
-            with nifgen.Session(
-                instrument_name,
-                options={"Simulate": True, "DriverSetup": {"Model": "5451", "BoardType": "PXIe"}},
-            ) as session:
+            with nifgen.Session("", options={"Simulate": True}) as session:
                 standalone_tsm_context.set_nifgen_session(instrument_name, session)
-                assert SemiconductorModuleContext._sessions[id(session)] is session
+                assert standalone_tsm_context._sessions[id(session)] is session
 
-    def test_get_all_nifgen_sessions(
-        self, standalone_tsm_context: SemiconductorModuleContext, simulated_nifgen_sessions
-    ):
+    def test_get_all_nifgen_sessions(self, standalone_tsm_context, simulated_nifgen_sessions):
         queried_sessions = standalone_tsm_context.get_all_nifgen_sessions()
         assert len(queried_sessions) == len(simulated_nifgen_sessions)
         for queried_session in queried_sessions:
             assert isinstance(queried_session, nifgen.Session)
             assert queried_session in simulated_nifgen_sessions
 
-    def test_pin_to_nifgen_session(
-        self, standalone_tsm_context: SemiconductorModuleContext, simulated_nifgen_sessions
-    ):
-        for system_pin in self.pin_map_system_pins:
-            (
-                pin_query_context,
-                queried_session,
-                queried_channel_list,
-            ) = standalone_tsm_context.pin_to_nifgen_session(system_pin)
-            assert isinstance(pin_query_context, NIFGenSinglePinSingleSessionQueryContext)
-            assert isinstance(queried_session, nifgen.Session)
-            assert isinstance(queried_channel_list, str)
-            assert queried_session in simulated_nifgen_sessions
+    def test_pin_to_nifgen_session(self, standalone_tsm_context, simulated_nifgen_sessions):
+        (
+            pin_query_context,
+            queried_session,
+            queried_channel_list,
+        ) = standalone_tsm_context.pin_to_nifgen_session("SystemPin1")
+        assert isinstance(pin_query_context, NIFGenSinglePinSingleSessionQueryContext)
+        assert isinstance(queried_session, nifgen.Session)
+        assert isinstance(queried_channel_list, str)
+        assert queried_session in simulated_nifgen_sessions
 
-    def test_pin_to_nifgen_sessions(
-        self, standalone_tsm_context: SemiconductorModuleContext, simulated_nifgen_sessions
-    ):
+    def test_pin_to_nifgen_sessions(self, standalone_tsm_context, simulated_nifgen_sessions):
         (
             pin_query_context,
             queried_sessions,
@@ -92,23 +72,18 @@ class TestNIFGen:
             assert isinstance(queried_channel_list, str)
             assert queried_session in simulated_nifgen_sessions
 
-    def test_pins_to_nifgen_session(
-        self, standalone_tsm_context: SemiconductorModuleContext, simulated_nifgen_sessions
-    ):
-        all_pins = self.pin_map_system_pins
+    def test_pins_to_nifgen_session(self, standalone_tsm_context, simulated_nifgen_sessions):
         (
             pin_query_context,
             queried_session,
             queried_channel_list,
-        ) = standalone_tsm_context.pins_to_nifgen_session(all_pins)
+        ) = standalone_tsm_context.pins_to_nifgen_session(self.pin_map_system_pins)
         assert isinstance(pin_query_context, NIFGenMultiplePinSingleSessionQueryContext)
         assert isinstance(queried_session, nifgen.Session)
         assert isinstance(queried_channel_list, str)
         assert queried_session in simulated_nifgen_sessions
 
-    def test_pins_to_nifgen_sessions(
-        self, standalone_tsm_context: SemiconductorModuleContext, simulated_nifgen_sessions
-    ):
+    def test_pins_to_nifgen_sessions(self, standalone_tsm_context, simulated_nifgen_sessions):
         all_pins = self.pin_map_dut_pins + self.pin_map_system_pins
         (
             pin_query_context,
