358eeaf2dde6516730fa5c4669fe56b11be69016
smooresni <sean.moore@ni.com>
2021-02-02 14:07:36 -0600

    reduce pinquerycontexts down to 1 class

--- src/nitsm/pinquerycontexts.py
+++ src/nitsm/pinquerycontexts.py
@@ -4,185 +4,69 @@ Pin Query Contexts
 
 import typing
 
-__all__ = [
-    "NIDCPowerSinglePinSingleSessionQueryContext",
-    "NIDCPowerSinglePinMultipleSessionQueryContext",
-    "NIDCPowerMultiplePinSingleSessionQueryContext",
-    "NIDCPowerMultiplePinMultipleSessionQueryContext",
-    "NIDmmSinglePinSingleSessionQueryContext",
-    "NIDmmSinglePinMultipleSessionQueryContext",
-    "NIDmmMultiplePinMultipleSessionQueryContext",
-    "NIFGenSinglePinSingleSessionQueryContext",
-    "NIFGenSinglePinMultipleSessionQueryContext",
-    "NIFGenMultiplePinSingleSessionQueryContext",
-    "NIFGenMultiplePinMultipleSessionQueryContext",
-    "NIScopeSinglePinSingleSessionQueryContext",
-    "NIScopeSinglePinMultipleSessionQueryContext",
-    "NIScopeMultiplePinSingleSessionQueryContext",
-    "NIScopeMultiplePinMultipleSessionQueryContext",
-    "NIDAQmxSinglePinSingleTaskQueryContext",
-    "NIDAQmxSinglePinMultipleTaskQueryContext",
-    "NIDAQmxMultiplePinSingleTaskQueryContext",
-    "NIDAQmxMultiplePinMultipleTaskQueryContext",
-    "NIDigitalPatternPinQueryContext",
-    "NIDigitalPatternSingleSessionPinQueryContext",
-]
-
+__all__ = ["PinQueryContext"]
 
 if typing.TYPE_CHECKING:
     import nitsm.pinmapinterfaces
 
 
 class PinQueryContext:
-    def __init__(self, tsm_context: "nitsm.pinmapinterfaces.ISemiconductorModuleContext"):
+    def __init__(self, tsm_context: "nitsm.pinmapinterfaces.ISemiconductorModuleContext", pins):
         self._tsm_context = tsm_context
-
-
-class SinglePinQueryContext(PinQueryContext):
-    def __init__(self, tsm_context, pin):
-        self.pin = pin
-        super().__init__(tsm_context)
-
-
-class MultiplePinQueryContext(PinQueryContext):
-    def __init__(self, tsm_context, pins):
-        self.pins = pins
-        super().__init__(tsm_context)
-
-
-class SinglePinSingleSessionQueryContext(SinglePinQueryContext):
-    def publish_float_scalar(self, data, published_data_id=""):
-        return self.publish_float_1d([data], published_data_id)
-
-    def publish_float_1d(self, data, published_data_id=""):
-        return self._tsm_context.Publish(self.pin, published_data_id, data)
-
-    def publish_bool_scalar(self, data, published_data_id=""):
-        return self.publish_bool_1d([data], published_data_id)
-
-    def publish_bool_1d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_5(self.pin, published_data_id, data)
-
-
-class SinglePinMultipleSessionQueryContext(SinglePinQueryContext):
-    def publish_float_1d(self, data, published_data_id=""):
-        transposed_data = [[val] for val in data]
-        return self.publish_float_2d(transposed_data, published_data_id)
-
-    def publish_float_2d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_3(self.pin, published_data_id, data)
-
-    def publish_bool_1d(self, data, published_data_id=""):
-        transposed_data = [[val] for val in data]
-        return self.publish_bool_2d(transposed_data, published_data_id)
-
-    def publish_bool_2d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_7(self.pin, published_data_id, data)
-
-
-class MultiplePinSingleSessionQueryContext(MultiplePinQueryContext):
-    def publish_float_1d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_2(self.pins, published_data_id, data)
-
-    def publish_bool_1d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_6(self.pins, published_data_id, data)
-
-
-class MultiplePinMultipleSessionQueryContext(MultiplePinQueryContext):
-    def publish_float_1d(self, data, published_data_id=""):
-        transposed_data = [[val] for val in data]
-        return self.publish_float_2d(transposed_data, published_data_id)
-
-    def publish_float_2d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_4(self.pins, published_data_id, data)
-
-    def publish_bool_1d(self, data, published_data_id=""):
-        transposed_data = [[val] for val in data]
-        return self.publish_bool_2d(transposed_data, published_data_id)
-
-    def publish_bool_2d(self, data, published_data_id=""):
-        return self._tsm_context.Publish_8(self.pins, published_data_id, data)
-
-
-class NIDCPowerSinglePinSingleSessionQueryContext(SinglePinSingleSessionQueryContext):
-    pass
-
-
-class NIDCPowerSinglePinMultipleSessionQueryContext(SinglePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDCPowerMultiplePinSingleSessionQueryContext(MultiplePinSingleSessionQueryContext):
-    pass
-
-
-class NIDCPowerMultiplePinMultipleSessionQueryContext(MultiplePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDmmSinglePinSingleSessionQueryContext(SinglePinSingleSessionQueryContext):
-    pass
-
-
-class NIDmmSinglePinMultipleSessionQueryContext(SinglePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDmmMultiplePinMultipleSessionQueryContext(MultiplePinMultipleSessionQueryContext):
-    pass
-
-
-class NIFGenSinglePinSingleSessionQueryContext(SinglePinSingleSessionQueryContext):
-    pass
-
-
-class NIFGenSinglePinMultipleSessionQueryContext(SinglePinMultipleSessionQueryContext):
-    pass
-
-
-class NIFGenMultiplePinSingleSessionQueryContext(MultiplePinSingleSessionQueryContext):
-    pass
-
-
-class NIFGenMultiplePinMultipleSessionQueryContext(MultiplePinMultipleSessionQueryContext):
-    pass
-
-
-class NIScopeSinglePinSingleSessionQueryContext(SinglePinSingleSessionQueryContext):
-    pass
-
-
-class NIScopeSinglePinMultipleSessionQueryContext(SinglePinMultipleSessionQueryContext):
-    pass
-
-
-class NIScopeMultiplePinSingleSessionQueryContext(MultiplePinSingleSessionQueryContext):
-    pass
-
-
-class NIScopeMultiplePinMultipleSessionQueryContext(MultiplePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDAQmxSinglePinSingleTaskQueryContext(SinglePinSingleSessionQueryContext):
-    pass
-
-
-class NIDAQmxSinglePinMultipleTaskQueryContext(SinglePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDAQmxMultiplePinSingleTaskQueryContext(MultiplePinSingleSessionQueryContext):
-    pass
-
-
-class NIDAQmxMultiplePinMultipleTaskQueryContext(MultiplePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDigitalPatternPinQueryContext(MultiplePinMultipleSessionQueryContext):
-    pass
-
-
-class NIDigitalPatternSingleSessionPinQueryContext(MultiplePinSingleSessionQueryContext):
-    pass
+        self._pins = pins
+
+    @property
+    def pins(self):
+        return self._pins
+
+    def publish(self, data, published_data_id=""):
+        if isinstance(data, float):
+            return self._publish_float_scalar(data, published_data_id)
+        elif isinstance(data, bool):
+            return self._publish_bool_scalar(data, published_data_id)
+        else:
+            return self._publish_sequence(data, published_data_id)
+
+    def _publish_float_scalar(self, data, published_data_id):
+        return self._publish_float_1d([data], published_data_id)
+
+    def _publish_bool_scalar(self, data, published_data_id):
+        return self._publish_bool_1d([data], published_data_id)
+
+    def _publish_sequence(self, data, published_data_id):
+        if isinstance(data[0], float):
+            return self._publish_float_1d(data, published_data_id)
+        elif isinstance(data[0], bool):
+            return self._publish_bool_1d(data, published_data_id)
+        else:
+            return self._publish_sequence_2d(data, published_data_id)
+
+    def _publish_float_1d(self, data, published_data_id):
+        if isinstance(self._pins, str):
+            return self._tsm_context.Publish(self._pins, published_data_id, data)
+        else:
+            return self._tsm_context.Publish_2(self._pins, published_data_id, data)
+
+    def _publish_bool_1d(self, data, published_data_id):
+        if isinstance(self._pins, str):
+            return self._tsm_context.Publish_5(self._pins, published_data_id, data)
+        else:
+            return self._tsm_context.Publish_6(self._pins, published_data_id, data)
+
+    def _publish_sequence_2d(self, data, published_data_id):
+        if isinstance(data[0][0], float):
+            return self._publish_float_2d(data, published_data_id)
+        else:
+            return self._publish_bool_2d(data, published_data_id)
+
+    def _publish_float_2d(self, data, published_data_id):
+        if isinstance(self._pins, str):
+            return self._tsm_context.Publish_3(self._pins, published_data_id, data)
+        else:
+            return self._tsm_context.Publish_4(self._pins, published_data_id, data)
+
+    def _publish_bool_2d(self, data, published_data_id):
+        if isinstance(self._pins, str):
+            return self._tsm_context.Publish_7(self._pins, published_data_id, data)
+        else:
+            return self._tsm_context.Publish_8(self._pins, published_data_id, data)
