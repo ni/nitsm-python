2213247d64ea03a201ad6a0d25e1861def689605
Sean Moore <sean.moore@ni.com>
2021-07-28 09:02:32 -0500

    allow code_module decorator to be used in classes

--- src/nitsm/codemoduleapi.py
+++ src/nitsm/codemoduleapi.py
@@ -1,20 +1,40 @@
+import inspect
 import functools
 from .tsmcontext import SemiconductorModuleContext
 from .enums import Capability, InstrumentTypeIdConstants
 
-__all__ = ["SemiconductorModuleContext", "Capability", "InstrumentTypeIdConstants"]
+__all__ = ["SemiconductorModuleContext", "Capability", "InstrumentTypeIdConstants", "code_module"]
 
 
-def code_module(callable_):
-    """
-    Converts the Semiconductor Module context passed from TestStand into a native Python object. The
-    Semiconductor Module context must be the first argument to the function.
-    """
+# noinspection PyPep8Naming
+class code_module:
+    def __init__(self, callable_):
+        """
+        Converts the Semiconductor Module context passed from TestStand into a native Python object.
+        The Semiconductor Module context must be the first positional argument to the function or
+        method.
+        """
 
-    @functools.wraps(callable_)
-    def nitsm_code_module(tsm_context, *args, **kwargs):
+        self._callable = callable_
+        functools.update_wrapper(self, callable_)
+
+    def __get__(self, instance, owner):
+        callable_ = self._callable.__get__(instance, owner)
+        return self.__class__(callable_)
+
+    def __call__(self, *args, **kwargs):
+        try:
+            index = int(inspect.isclass(args[0]))  # class method check
+            tsm_context = args[index]
+        except IndexError:
+            raise TypeError("Invalid number of positional arguments.", self._callable)
         if not isinstance(tsm_context, SemiconductorModuleContext):
-            tsm_context = SemiconductorModuleContext(tsm_context)
-        return callable_(tsm_context, *args, **kwargs)
-
-    return nitsm_code_module
+            try:
+                tsm_context = SemiconductorModuleContext(tsm_context)
+            except Exception:
+                class_name = tsm_context.__class__.__name__
+                raise ValueError(
+                    f"Failed to convert Semiconductor Module context from class '{class_name}'."
+                )
+            args = *args[:index], tsm_context, *args[index + 1 :]  # noqa E203
+        return self._callable(*args, **kwargs)
---
+++ tests/test_codemoduleapi.py
@@ -0,0 +1,71 @@
+import pytest
+from nitsm.codemoduleapi import code_module, SemiconductorModuleContext
+
+
+@pytest.mark.pin_map("empty.pinmap")
+class TestCodeModuleApi:
+    """
+    Pytest passes fixtures as keyword arguments while the code_module decorator expects the TSM
+    context to be passed as a positional argument. Therefore, we must redirect the keyword argument
+    to a positional argument by creating surrogate methods.
+    """
+
+    @staticmethod
+    @code_module
+    def _static_method(tsm_context):
+        assert isinstance(tsm_context, SemiconductorModuleContext)
+
+    def test_static_method(self, standalone_tsm_context_com_object):
+        return TestCodeModuleApi._static_method(standalone_tsm_context_com_object)
+
+    @code_module
+    def _instance_method(self, tsm_context):
+        assert isinstance(tsm_context, SemiconductorModuleContext)
+
+    def test_instance_method(self, standalone_tsm_context_com_object):
+        return self._instance_method(standalone_tsm_context_com_object)
+
+    @classmethod
+    @code_module
+    def _class_method(cls, tsm_context):
+        assert issubclass(cls, TestCodeModuleApi)
+        assert isinstance(tsm_context, SemiconductorModuleContext)
+
+    def test_class_method(cls, standalone_tsm_context_com_object):
+        return TestCodeModuleApi._class_method(standalone_tsm_context_com_object)
+
+    @code_module
+    def _invalid_number_of_positional_arguments(self):
+        """Does not contain a positional argument for the TSM context."""
+        assert False  # should not reach this point
+
+    def test_invalid_number_of_positional_arguments(self, standalone_tsm_context_com_object):
+        with pytest.raises(TypeError) as e:
+            self._invalid_number_of_positional_arguments()
+        assert e.value.args[0] == "Invalid number of positional arguments."
+        assert e.value.args[1] == self._invalid_number_of_positional_arguments._callable
+
+    @code_module
+    def _tsm_context_not_first_positional_argument(self, something_else, tsm_context):
+        assert False  # should not reach this point
+
+    @pytest.mark.parametrize("first_argument", (None, int(), str()))
+    def test_tsm_context_not_first_positional_argument(
+        self, first_argument, standalone_tsm_context_com_object
+    ):
+        with pytest.raises(Exception) as e:
+            self._tsm_context_not_first_positional_argument(
+                first_argument, standalone_tsm_context_com_object
+            )
+        assert e.value.args[0].startswith(
+            "Failed to convert Semiconductor Module context from class"
+        )
+
+
+@pytest.mark.pin_map("empty.pinmap")
+def test_function(standalone_tsm_context_com_object):
+    @code_module
+    def func(tsm_context):
+        assert isinstance(tsm_context, SemiconductorModuleContext)
+
+    return func(standalone_tsm_context_com_object)
