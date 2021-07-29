fa916e4a59e4fc59149f613c77ed9a81d6ae0a74
Sean Moore <sean.moore@ni.com>
2021-07-29 08:25:27 -0500

    use inspect module to bind arguments

--- src/nitsm/codemoduleapi.py
+++ src/nitsm/codemoduleapi.py
@@ -11,11 +11,11 @@ class code_module:
     def __init__(self, callable_):
         """
         Converts the Semiconductor Module context passed from TestStand into a native Python object.
-        The Semiconductor Module context must be the first positional argument to the function or
-        method.
+        The Semiconductor Module context must be the first argument to the function or method.
         """
 
         self._callable = callable_
+        self._signature = inspect.signature(callable_)
         functools.update_wrapper(self, callable_)
 
     def __get__(self, instance, owner):
@@ -23,18 +23,33 @@ class code_module:
         return self.__class__(callable_)
 
     def __call__(self, *args, **kwargs):
+        bound_arguments = self._signature.bind(*args, **kwargs)
+        arguments_iter = iter(bound_arguments.arguments.items())
+
+        # find potential argument that could be the tsm context
         try:
-            index = int(inspect.isclass(args[0]))  # class method check
-            tsm_context = args[index]
-        except IndexError:
-            raise TypeError("Invalid number of positional arguments.", self._callable)
-        if not isinstance(tsm_context, SemiconductorModuleContext):
-            try:
-                tsm_context = SemiconductorModuleContext(tsm_context)
-            except Exception:
-                class_name = tsm_context.__class__.__name__
-                raise ValueError(
-                    f"Failed to convert Semiconductor Module context from class '{class_name}'."
+            argument = next(arguments_iter)  # get first argument
+            if inspect.isclass(argument[1]):  # class method check
+                argument = next(arguments_iter)  # move to second argument
+        except StopIteration:
+            raise TypeError(
+                (
+                    "The number of arguments to the code module is less expected. It must accept "
+                    "as it's first argument the Semiconductor Module context passed from TestStand "
+                    "or another code module.",
                 )
-            args = *args[:index], tsm_context, *args[index + 1 :]  # noqa: E203
-        return self._callable(*args, **kwargs)
+            )
+
+        # attempt to wrap argument in a SemiconductorModuleContext class
+        argument_name, argument_value = argument
+        if not isinstance(argument_value, SemiconductorModuleContext):
+            try:
+                argument_value = SemiconductorModuleContext(argument_value)
+            except Exception:
+                class_name = argument_value.__class__.__name__
+                raise ValueError(
+                    f"Failed to convert Semiconductor Module context from class '{class_name}'.",
+                )
+            bound_arguments.arguments[argument_name] = argument_value
+
+        return self._callable(*bound_arguments.args, **bound_arguments.kwargs)
--- tests/test_codemoduleapi.py
+++ tests/test_codemoduleapi.py
@@ -1,53 +1,57 @@
+import re
 import pytest
 from nitsm.codemoduleapi import code_module, SemiconductorModuleContext
 
 
 @pytest.mark.pin_map("empty.pinmap")
 class TestCodeModuleApi:
-    """
-    Pytest passes fixtures as keyword arguments while the code_module decorator expects the TSM
-    context to be passed as a positional argument. Therefore, we must redirect the keyword argument
-    to a positional argument by creating surrogate methods.
-    """
+    @staticmethod
+    @code_module
+    def test_static_method_converts(standalone_tsm_context_com_object):
+        assert isinstance(standalone_tsm_context_com_object, SemiconductorModuleContext)
 
     @staticmethod
     @code_module
-    def _static_method(tsm_context):
-        assert isinstance(tsm_context, SemiconductorModuleContext)
-
-    def test_static_method(self, standalone_tsm_context_com_object):
-        return TestCodeModuleApi._static_method(standalone_tsm_context_com_object)
+    def test_static_method_does_not_convert(standalone_tsm_context):
+        assert isinstance(standalone_tsm_context, SemiconductorModuleContext)
 
     @code_module
-    def _instance_method(self, tsm_context):
-        assert isinstance(tsm_context, SemiconductorModuleContext)
+    def test_instance_method_converts(self, standalone_tsm_context_com_object):
+        assert isinstance(standalone_tsm_context_com_object, SemiconductorModuleContext)
 
-    def test_instance_method(self, standalone_tsm_context_com_object):
-        return self._instance_method(standalone_tsm_context_com_object)
+    @code_module
+    def test_instance_method_does_not_convert(self, standalone_tsm_context):
+        assert isinstance(standalone_tsm_context, SemiconductorModuleContext)
 
     @classmethod
     @code_module
-    def _class_method(cls, tsm_context):
+    def test_class_method_converts(cls, standalone_tsm_context_com_object):
         assert issubclass(cls, TestCodeModuleApi)
-        assert isinstance(tsm_context, SemiconductorModuleContext)
+        assert isinstance(standalone_tsm_context_com_object, SemiconductorModuleContext)
 
-    def test_class_method(self, standalone_tsm_context_com_object):
-        return TestCodeModuleApi._class_method(standalone_tsm_context_com_object)
+    @classmethod
+    @code_module
+    def test_class_method_does_not_convert(cls, standalone_tsm_context):
+        assert issubclass(cls, TestCodeModuleApi)
+        assert isinstance(standalone_tsm_context, SemiconductorModuleContext)
 
     @code_module
     def _invalid_number_of_positional_arguments(self):
         """Does not contain a positional argument for the TSM context."""
         assert False  # should not reach this point
 
-    def test_invalid_number_of_positional_arguments(self, standalone_tsm_context_com_object):
+    def test_invalid_number_of_positional_arguments(self):
         with pytest.raises(TypeError) as e:
             self._invalid_number_of_positional_arguments()
-        assert e.value.args[0] == "Invalid number of positional arguments."
-        assert e.value.args[1] == self._invalid_number_of_positional_arguments._callable
+        assert e.value.args[0] == (
+            "The number of arguments to the code module is less expected. It must accept "
+            "as it's first argument the Semiconductor Module context passed from TestStand "
+            "or another code module.",
+        )
 
     # noinspection PyUnusedLocal
     @code_module
-    def _tsm_context_not_first_positional_argument(self, something_else, tsm_context):
+    def _tsm_context_not_first_positional_argument(self, first_argument, tsm_context):
         assert False  # should not reach this point
 
     @pytest.mark.parametrize("first_argument", (None, int(), str()))
@@ -58,23 +62,26 @@ class TestCodeModuleApi:
             self._tsm_context_not_first_positional_argument(
                 first_argument, standalone_tsm_context_com_object
             )
-        assert e.value.args[0].startswith(
-            "Failed to convert Semiconductor Module context from class"
+        assert re.match(
+            r"Failed to convert Semiconductor Module context from class '.*'\.",
+            e.value.args[0],
         )
 
-    # noinspection PyUnusedLocal
+    @pytest.mark.parametrize("second_argument", (None,))
     @code_module
-    def _positional_arguments_after_tsm_context(self, tsm_context, *args):
-        assert isinstance(tsm_context, SemiconductorModuleContext)
-
-    def test_positional_arguments_after_tsm_context(self, standalone_tsm_context_com_object):
-        self._positional_arguments_after_tsm_context(standalone_tsm_context_com_object, None)
+    def test_positional_arguments_after_tsm_context(
+        self, standalone_tsm_context_com_object, second_argument
+    ):
+        assert isinstance(standalone_tsm_context_com_object, SemiconductorModuleContext)
 
 
 @pytest.mark.pin_map("empty.pinmap")
-def test_function(standalone_tsm_context_com_object):
-    @code_module
-    def func(tsm_context):
-        assert isinstance(tsm_context, SemiconductorModuleContext)
+@code_module
+def test_function_converts(standalone_tsm_context_com_object):
+    assert isinstance(standalone_tsm_context_com_object, SemiconductorModuleContext)
 
-    return func(standalone_tsm_context_com_object)
+
+@pytest.mark.pin_map("empty.pinmap")
+@code_module
+def test_function_does_not_convert(standalone_tsm_context):
+    assert isinstance(standalone_tsm_context, SemiconductorModuleContext)
