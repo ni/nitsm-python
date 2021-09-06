f9ab26e30fde6618ee314552eaf76bd465466a17
Sean Moore <sean.moore@ni.com>
2021-09-05 21:59:18 -0500

    make pinmapinterfaces private

--- tools/makepy_pinmapinterfaces.py
+++ tools/makepy_pinmapinterfaces.py
@@ -2,7 +2,7 @@ import sys
 import os.path
 from win32com.client import makepy
 
-output_file = os.path.join(os.path.dirname(__file__), "pinmapinterfaces.py")
+output_file = os.path.join(os.path.dirname(__file__), "_pinmapinterfaces.py")
 pmi_type_library = (
     r"C:\Program Files\National Instruments\TestStand 2020\Bin"
     r"\NationalInstruments.TestStand.SemiconductorModule.PinMapInterfaces.tlb"
