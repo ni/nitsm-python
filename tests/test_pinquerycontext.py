ddb25152baf7b21b4f2a033a8d1dd99c04b7a82c
Sean Moore <sean.moore@ni.com>
2021-09-16 08:11:03 -0500

    simplify channel groups with nidigital sessions

--- tests/test_pinquerycontext.py
+++ tests/test_pinquerycontext.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+import nidigital
 import pytest
 
 if TYPE_CHECKING:
@@ -6,27 +7,29 @@ if TYPE_CHECKING:
 
 
 @pytest.fixture
-def pseudo_sessions(standalone_tsm_context):
-    names, groups, channels = standalone_tsm_context.get_custom_instrument_names(
-        "CustomInstrumentTypeId1"
-    )
-    for name, group, channels in zip(names, groups, channels):
-        standalone_tsm_context.set_custom_session("CustomInstrumentTypeId1", name, group, channels)
+def simulated_nidigital_sessions(standalone_tsm_context):
+    instrument_names = standalone_tsm_context.get_all_nidigital_instrument_names()
+    sessions = [
+        nidigital.Session(
+            instrument_name, options={"Simulate": True, "driver_setup": {"Model": "6570"}}
+        )
+        for instrument_name in instrument_names
+    ]
+    for instrument_name, session in zip(instrument_names, sessions):
+        standalone_tsm_context.set_nidigital_session(instrument_name, session)
+    yield sessions
+    for session in sessions:
+        session.close()
 
 
 @pytest.mark.pin_map("pinquerycontext.pinmap")
 class TestPinQueryContext:
-    @pytest.mark.usefixtures("pseudo_sessions")
+    @pytest.mark.usefixtures("simulated_nidigital_sessions")
     def test_get_session_and_channel_index(
         self, standalone_tsm_context: "SemiconductorModuleContext"
     ):
-        (
-            pin_query_context,
-            session_data,
-            channel_group,
-            channel_list,
-        ) = standalone_tsm_context.pins_to_custom_sessions(
-            "CustomInstrumentTypeId1", ["DUTPin2", "DUTPin1"]
+        pin_query_context, _, _ = standalone_tsm_context.pins_to_nidigital_sessions_for_ppmu(
+            ["DUTPin2", "DUTPin1"]
         )
         session_index, channel_index = pin_query_context.get_session_and_channel_index(1, "DUTPin1")
         assert session_index == 1
