20c4a952b4a837227c4df69a06962da870e5d30b
Sean Moore <sean.moore@ni.com>
2021-09-23 13:35:25 -0500

    define test data inline

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -1,7 +1,4 @@
-import re
 import random
-
-import collections
 import pytest
 from nitsm.codemoduleapi import SemiconductorModuleContext
 
@@ -74,14 +71,14 @@ class TestSinglePin1d:
             session,
             site_list,
         ) = standalone_tsm_context.pins_to_nidigital_session_for_pattern(self._PIN)
-        num_sites = len(site_list.split(","))
-        # alternate True and False
-        test_data = collections.OrderedDict((i, bool(i % 2)) for i in range(num_sites))
+        test_data = {1: False, 0: True, 2: True}  # alternate True and False
         pin_query_context.publish_pattern_results(test_data)
         published_data = published_data_reader.get_and_clear_published_data()
         assert len(published_data) == len(test_data)
-        for published_data_point, test_data_point in zip(published_data, test_data.values()):
-            assert published_data_point.boolean_value == test_data_point
+        for published_data_point, site_number in zip(
+            published_data, standalone_tsm_context.site_numbers
+        ):
+            assert published_data_point.boolean_value == test_data[site_number]
 
 
 @pytest.mark.pin_map("publish.pinmap")
@@ -123,15 +120,7 @@ class TestSinglePin2d:
             site_lists,
         ) = standalone_tsm_context.pins_to_nidigital_sessions_for_pattern(self._PIN)
         expected_results = [True, False, True]  # test data across sites [0, 1, 2]
-        pattern = re.compile(r"\s*site(\d+)")
-        site_lists = [
-            [int(match[1]) for match in map(pattern.match, site_list.split(","))]
-            for site_list in site_lists
-        ]
-        test_data = [  # creates data for purely jagged results [[site0], [site1, site2]]
-            collections.OrderedDict((site, expected_results[site]) for site in site_list)
-            for site_list in site_lists
-        ]
+        test_data = [{0: True}, {2: True, 1: False}]
         pin_query_context.publish_pattern_results(test_data)
         published_data = published_data_reader.get_and_clear_published_data()
         assert len(published_data) == len(expected_results)
@@ -174,14 +163,14 @@ class TestMultiplePins1d:
             session,
             site_list,
         ) = standalone_tsm_context.pins_to_nidigital_session_for_pattern(self._PINS)
-        num_sites = len(site_list.split(","))
-        # alternate True and False
-        test_data = collections.OrderedDict((i, bool(i % 2)) for i in range(num_sites))
+        test_data = {1: False, 0: True, 2: True}
         pin_query_context.publish_pattern_results(test_data)
         published_data = published_data_reader.get_and_clear_published_data()
         assert len(published_data) == len(test_data)
-        for published_data_point, test_data_point in zip(published_data, test_data.values()):
-            assert published_data_point.boolean_value == test_data_point
+        for published_data_point, site_number in zip(
+            published_data, standalone_tsm_context.site_numbers
+        ):
+            assert published_data_point.boolean_value == test_data[site_number]
 
 
 @pytest.mark.pin_map("publish.pinmap")
@@ -223,7 +212,7 @@ class TestMultiplePins2d:
             site_lists,
         ) = standalone_tsm_context.pins_to_nidigital_sessions_for_pattern(self._PINS)
         # [DigitalPattern1: site0, site1, site2], [DigitalPattern2: site1, site2]
-        test_data = [{0: True, 1: False, 2: True}, {1: True, 2: True}]
+        test_data = [{1: False, 0: True, 2: True}, {1: True, 2: True}]
         expected_results = [True, False, True]  # test_data AND'd across site
         pin_query_context.publish_pattern_results(test_data)
         published_data = published_data_reader.get_and_clear_published_data()
