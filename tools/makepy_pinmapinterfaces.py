4d81a00efa40bf0b9637702689a18b4b9a0f3a5c
Luis Gomes <luis.gomes@ni.com>
2022-05-12 13:37:24 -0500

    Don't hardcode TestStand version when creating PinMapInterfaces com wrapper

--- tools/makepy_pinmapinterfaces.py
+++ tools/makepy_pinmapinterfaces.py
@@ -1,12 +1,15 @@
 import sys
 import os.path
+import winreg
 from win32com.client import makepy
 
+def get_env_variable_from_registry(variable_name):
+    key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, r"System\CurrentControlSet\Control\Session Manager\Environment")
+    return winreg.QueryValueEx(key, variable_name)[0]
+
 output_file = os.path.join(os.path.dirname(__file__), "_pinmapinterfaces.py")
-pmi_type_library = (
-    r"C:\Program Files\National Instruments\TestStand 2020\Bin"
-    r"\NationalInstruments.TestStand.SemiconductorModule.PinMapInterfaces.tlb"
-)
+_teststand_public_path = get_env_variable_from_registry("TestStandPublic64")
+pmi_type_library = os.path.join(_teststand_public_path,  "Bin", "NationalInstruments.TestStand.SemiconductorModule.PinMapInterfaces.tlb")
 
 sys.argv = ["makepy", "-o", output_file, pmi_type_library]
 
