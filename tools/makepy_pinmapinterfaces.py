3a3f2c29ce82e9789fd7ab56e6b17cea8b3c3dd5
Luis Gomes <luis.gomes@ni.com>
2022-05-13 11:04:59 -0500

    Get environment variable from the process environment rather than from the registry, since makepy doesn't run in the pipeline

--- tools/makepy_pinmapinterfaces.py
+++ tools/makepy_pinmapinterfaces.py
@@ -4,17 +4,10 @@ import winreg
 from win32com.client import makepy
 
 
-def get_env_variable_from_registry(variable_name):
-    key = winreg.CreateKey(
-        winreg.HKEY_LOCAL_MACHINE, r"System\CurrentControlSet\Control\Session Manager\Environment"
-    )
-    return winreg.QueryValueEx(key, variable_name)[0]
-
-
 output_file = os.path.join(os.path.dirname(__file__), "_pinmapinterfaces.py")
-_teststand_public_path = get_env_variable_from_registry("TestStandPublic64")
+teststand_public_path = os.environ["TestStandPublic64"]
 pmi_type_library = os.path.join(
-    _teststand_public_path,
+    teststand_public_path,
     "Bin",
     "NationalInstruments.TestStand.SemiconductorModule.PinMapInterfaces.tlb",
 )
